876470d12a8483179eea6bb101daae7ed939d7d2
Mabrouk Ali <mabroukali025@gmail.com>
2023-12-30 16:59:26 +0100

    Update ProduitDao.java
    modefication .md

--- src/main/java/ma/project/siteecommerce/dao/ProduitDao.java
+++ src/main/java/ma/project/siteecommerce/dao/ProduitDao.java
@@ -1,6 +1,5 @@
 package ma.project.siteecommerce.dao;
 
-import ma.project.siteecommerce.entites.Facture;
 import ma.project.siteecommerce.entites.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
